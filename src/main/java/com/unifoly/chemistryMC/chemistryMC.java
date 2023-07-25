f08c3ed342787a7c9ae67481122d5b5ff97ffbb2
unifoly <xuran080705@163.com>
2023-07-25 08:15:27 +0800

    ‘beginning’

---
+++ src/main/java/com/unifoly/chemistryMC/chemistryMC.java
@@ -0,0 +1,14 @@
+package com.unifoly.chemistryMC;
+
+import com.unifoly.chemistryMC.utils.utils;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author unifoly
+ * {@code @date}  2023/7/25 7:59
+ * @version 1.0
+ */
+
+@Mod(utils.MOD_ID)
+public class chemistryMC {
+}
---
+++ src/main/java/com/unifoly/chemistryMC/utils/utils.java
@@ -0,0 +1,10 @@
+package com.unifoly.chemistryMC.utils;
+
+/**
+ * @author unifoly
+ * {@code @date}  2023/7/25 7:57
+ * @version 1.0
+ */
+public class utils {
+    public static final String MOD_ID = "chemistrymc";
+}
